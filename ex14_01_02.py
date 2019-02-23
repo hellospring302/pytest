ac66f6882752d886ef55af42a8546642b136ff68
hellospring302 <1285953778@qq.com>
2019-02-23 13:48:18 +0800

    修改

--- ex14_01_02.py
+++ ex14_01_02.py
@@ -4,7 +4,7 @@ from docx import Document
 from docx.shared import RGBColor, Pt
 doc = Document("test14_01.docx")
 p = doc.add_paragraph()
-run = p.add_run('本内容由马森杰同学修改')
+run = p.add_run('本内容由XXX同学修改')
 run.bold = True
 run.font.size = Pt(16)
 run.font.color.rgb = RGBColor(0,0,255)
